8fc9b0843481ddfd8e524c5b2aca4d40074b12aa
vasyanovichk <35076134+vasyanovichk@users.noreply.github.com>
2019-01-09 21:39:23 +0200

    animated commit

--- alabom/js/main.js
+++ alabom/js/main.js
@@ -1,86 +1,56 @@
 
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50){  
+        $('.nav').addClass("sticky");
+    }
+    else{
+        $('.nav').removeClass("sticky");
+    }
+    if ($(this).scrollTop() > 400){  
+        $('.nav .nav__btn-shop').removeClass ("d-none");
+    }
+    else{
+        $('.nav .nav__btn-shop').addClass("d-none");
+    }
+});
 
-
- 
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 50){  
-            $('.nav').addClass("sticky");
-        }
-        else{
-            $('.nav').removeClass("sticky");
-        }
-        if ($(this).scrollTop() > 400){  
-            $('.nav .nav__btn-shop').removeClass ("d-none");
-        }
-        else{
-            $('.nav .nav__btn-shop').addClass("d-none");
+$(window).scroll(function (){
+    $('.play-rubberBand').each(function (){
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+650) {
+            $(this).addClass('rubberBand');
         }
     });
+});﻿
 
+$(window).scroll(function (){
+    $('.play-swing').each(function (){
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+650) {
+            $(this).addClass('swing');
+        }
+    });
+});﻿
 
-// $('.#parallax-bg3').scroolly([{
-//   from: 'doc-center',
-//   to: 'doc-center',
-//   css: {
-//     color: 'red'
-//   }
-// }]);
-
-
-
- // $(window).scroll(function() {
- //    if ($(this).scrollTop() > 50){  
- //           ($(this).offset(".next");
-
- //               });
-
+$(window).scroll(function (){
+    $('.play-bounce').each(function (){
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+650) {
+            $(this).addClass('bounce');
+        }
+    });
+});﻿
 
 $(function () {
-    // плавное перемещение страницы к нужному блоку
     $(".go").click(function (e) {
         e.preventDefault();
         elementClick = $(this).attr("href");
         destination = $(elementClick).offset().top;
         $("body,html").animate({scrollTop: destination }, 800);
     });
-
-
-
-
-
-  // $.scrollify({
-  //   section : ".scroll",
-  //   sectionName : "section-name",
-  //   easing: "easeOutExpo",
-  //   scrollSpeed: 1100,
-  //   offset : -100,
-  //   scrollbars: true,
-  //   before:function() {},
-  //   after:function() {}
-  // });
-
-
-
-
-
-//http://embed.plnkr.co/p2rt7UHiXRsRYxHCWImM/
-  // $.scrollify({
-  //   section : ".scroll",
-  //   sectionName : "section-name",
-  //   easing: "easeOutExpo",
-  //   scrollSpeed: 1100,
-  //   offset : -100,
-  //   scrollbars: true,
-  //   before:function() {},
-  //   after:function() {}
-  // });
-
-
-
-
-
-
-
 });
 
 

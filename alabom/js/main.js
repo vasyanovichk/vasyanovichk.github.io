93e261f9ba57bba0999a9b3c534588c1fb75dd56
vasyanovichk <35076134+vasyanovichk@users.noreply.github.com>
2019-01-10 17:01:12 +0200

    animated commit

--- alabom/js/main.js
+++ alabom/js/main.js
@@ -14,64 +14,6 @@ $(window).scroll(function() {
     }
 });
 
-// $(document).ready(function() {
-//   $(window).scroll(function() {
-//     if ($(document).scrollTop() > 900) {
-//       $(".play-rubberBand").addClass("d-none");
-//     } else {
-//       $(".play-rubberBand").removeClass("d-none");
-//     }
-//   });
-// });
-
-    $(window).scroll(function() {
-        $('.delay-right').each(function(){
-            var imagePos = $(this).offset().top;
-
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+950) {
-                $(this).addClass("fadeInRight");
-            }
-        });
-        $('.delay-left').each(function(){
-            var imagePos = $(this).offset().top;
-
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+950) {
-                $(this).addClass("fadeInLeft");
-            }
-        });
-    });
-
-$(window).scroll(function (){
-    $('.play-rubberBand').each(function (){
-        var imagePos = $(this).offset().top;
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow+650) {
-            $(this).addClass('rubberBand');
-        }
-    });
-});﻿
-
-$(window).scroll(function (){
-    $('.play-swing').each(function (){
-        var imagePos = $(this).offset().top;
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow+650) {
-            $(this).addClass('swing');
-        }
-    });
-});﻿
-
-$(window).scroll(function (){
-    $('.play-bounce').each(function (){
-        var imagePos = $(this).offset().top;
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow+650) {
-            $(this).addClass('bounce');
-        }
-    });
-});﻿
 
 $(function () {
     $(".go").click(function (e) {
@@ -82,5 +24,12 @@ $(function () {
     });
 });
 
+
+wow = new WOW(
+    {
+      mobile: false
+    }
+    )
+wow.init();
 
 

22475b5c0b495570374b0b3fa7416ae4ce6fb1f9
vasyanovichk <35076134+vasyanovichk@users.noreply.github.com>
2019-01-11 15:15:46 +0200

    btn commit

--- alabom/js/main.js
+++ alabom/js/main.js
@@ -1,37 +1,36 @@
-
-
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 50){  
-        $('.nav').addClass("sticky");
-    }
-    else{
-        $('.nav').removeClass("sticky");
-    }
-    if ($(this).scrollTop() > 400){  
-        $('.nav .nav__btn-shop').removeClass ("d-none");
-    }
-    else{
-        $('.nav .nav__btn-shop').addClass("d-none");
-    }
-});
-
-
-$(function () {
-    $(".go").click(function (e) {
-        e.preventDefault();
-        elementClick = $(this).attr("href");
-        destination = $(elementClick).offset().top;
-        $("body,html").animate({scrollTop: destination }, 800);
+jQuery(document).ready(function($) {
+    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50){  
+            $('.nav').addClass("sticky");
+        }
+        else{
+            $('.nav').removeClass("sticky");
+        }
+        if ($(this).scrollTop() > 400){  
+            $('.nav .nav__btn-shop').removeClass ("d-none");
+        }
+        else{
+            $('.nav .nav__btn-shop').addClass("d-none");
+        }
     });
+
+
+    $(function () {
+        $(".go").click(function (e) {
+            e.preventDefault();
+            elementClick = $(this).attr("href");
+            destination = $(elementClick).offset().top;
+            $("body,html").animate({scrollTop: destination }, 800);
+        });
+    });
+
+
+    wow = new WOW(
+        {
+          mobile: false
+        }
+        )
+    wow.init();
+
 });
-
-
-wow = new WOW(
-    {
-      mobile: false
-    }
-    )
-wow.init();
-
-
